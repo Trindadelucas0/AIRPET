f9545fb5ad3918b4dc7ff710eae748654f0d2e38
Lucas Rodrigues <175415849+Trindadelucas0@users.noreply.github.com>
2026-03-15 21:32:01 -0300

    z

--- src/routes/nfcRoutes.js
+++ src/routes/nfcRoutes.js
@@ -22,8 +22,34 @@ const upload = multer({
 router.get('/:tag_code', nfcController.processarScan);
 router.post('/:tag_code/localizacao', nfcController.registrarLocalizacaoPublica);
 router.get('/:tag_code/encontrei', nfcController.mostrarEncontrei);
-router.post('/:tag_code/encontrei', upload.single('foto'), nfcController.processarEncontrei);
+router.post('/:tag_code/encontrei', function (req, res, next) {
+  upload.single('foto')(req, res, function (err) {
+    if (err) {
+      const tagCode = req.params.tag_code;
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        if (req.session) req.session.flash = { tipo: 'erro', mensagem: 'Imagem muito grande. Tamanho máximo: 5 MB. Escolha outra foto ou envie sem foto.' };
+      } else {
+        if (req.session) req.session.flash = { tipo: 'erro', mensagem: 'Erro ao enviar a foto. Use JPEG, PNG, GIF ou WebP.' };
+      }
+      return res.redirect('/tag/' + encodeURIComponent(tagCode) + '/encontrei');
+    }
+    next();
+  });
+}, nfcController.processarEncontrei);
 router.get('/:tag_code/enviar-foto', nfcController.mostrarEnviarFoto);
-router.post('/:tag_code/enviar-foto', upload.single('foto'), nfcController.processarEnviarFoto);
+router.post('/:tag_code/enviar-foto', function (req, res, next) {
+  upload.single('foto')(req, res, function (err) {
+    if (err) {
+      const tagCode = req.params.tag_code;
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        if (req.session) req.session.flash = { tipo: 'erro', mensagem: 'Imagem muito grande. Tamanho máximo: 5 MB.' };
+      } else {
+        if (req.session) req.session.flash = { tipo: 'erro', mensagem: 'Erro ao enviar a foto. Use JPEG, PNG, GIF ou WebP.' };
+      }
+      return res.redirect('/tag/' + encodeURIComponent(tagCode) + '/enviar-foto');
+    }
+    next();
+  });
+}, nfcController.processarEnviarFoto);
 
 module.exports = router;
